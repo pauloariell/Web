25f4b67e410d7dea516af0d87a70683f052c0e57
pauloariell <pauloariell@live.com>
2017-03-13 18:04:16 -0300

    Add files via upload

---
+++ Filmes1.2/program.js
@@ -0,0 +1,23 @@
+function acao() {
+  var acao = document.querySelector("div.acao");
+  acao.style.order = "1";
+  document.body.article.getElementsByClassName(":not(.acao)").style.visibility = "hidden";
+}
+
+function drama() {
+  var drama = document.querySelector("div.drama");
+  acao.style.order = "1";
+  document.body.article.getElementsByClassName(":not(.drama)").style.visibility = "hidden";
+}
+
+function aventura() {
+  var aventura = document.querySelector("div.aventura");
+  acao.style.order = "1";
+  document.body.article.getElementsByClassName(":not(.aventura)").style.visibility = "hidden";
+}
+
+function vinganca() {
+  var aventura = document.querySelector("div.vinganca");
+  acao.style.order = "1";
+  document.body.article.getElementsByClassName(":not(.vinganca)").style.visibility = "hidden";
+}
